2ddbe13ad4acb8c2f96bbcdf18bdfab5c098518e
Finn Bauer <finn.bauer18@imperial.ac.uk>
2019-06-11 22:08:25 +0100

    Made object oriented

--- cashGraphs.py
+++ cashGraphs.py
@@ -2,110 +2,107 @@ import matplotlib.pyplot as plt
 import numpy as np
 from alpha_vantage.timeseries import TimeSeries
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import NearestNeighbors
+
+# Arguments: Underlying, range, period
+#
+class PriceData:
+    def __init__(self, ticker, range, period):
+        #For now period is one day everytime
+        #Input values
+        self._ticker = ticker
+        self._period = period
+        self._range = range
+
+        #Calculating the needed price data
+        self._prices, meta_data = self.get_price_data_daily(ticker)
+        intervals = self.calculate_intervals(self._prices, range)
+        self._normalized_intervals = self.normalise_time_series(intervals)
+        self._future_returns = self.calculate_future_returns_daily(self._prices, range)
+
+    def get_price_data_daily(self, ticker):
+        ts = TimeSeries(key='15BJH4XND0N7L3KL', output_format='pandas')
+        data, meta_data = ts.get_daily(symbol=ticker, outputsize='full')
+        return data['4. close'].values, meta_data
+
+    def calculate_intervals(self, prices, interval_length):
+        number_intervals = prices.shape[0] - interval_length
+        intervals = np.empty([number_intervals, interval_length])
+
+        interval = 0
+        iteration = 0
+        i = 0;
+
+        while(i < prices.shape[0]-1):
+            if(iteration == interval_length):
+                iteration = 0
+                interval += 1
+                i -= interval_length-1
+            intervals[interval][iteration] = prices[i]
+            iteration += 1
+            i += 1
+        return intervals
+
+    def normalise_time_series(self, time_series_matrix):
+        number_intervals = time_series_matrix.shape[0]
+        series_length = time_series_matrix.shape[1]
+        return_matrix = np.array(time_series_matrix, copy=True)
+
+        for i in range(number_intervals):
+            base = return_matrix[i][0]
+            for j in range(series_length):
+                return_matrix[i][j] = return_matrix[i][j] / base
+        return return_matrix
+
+    #Price Data is daily data
+    def calculate_future_returns_daily(self, prices, rangE):
+        number_intervals = prices.shape[0]
+        full_histories = number_intervals - 30
+        #Loop through the thing
+        future_returns = np.empty([full_histories, 4])
+
+        #Start the loop at the beginning of the second interval
+        i = 0
+
+        for j in range(rangE, full_histories, rangE):
+            #1, 2, 7, 30
+            future_returns[i][0] = prices[j+1] / prices[j]
+            future_returns[i][1] = prices[j+2] / prices[j]
+            future_returns[i][2] = prices[j+7] / prices[j]
+            future_returns[i][3] = prices[j+30] / prices[j]
+            i = i + 1
+
+        return future_returns
+
+class Neighbours:
+    def __init__(self, price_data, neighbours):
+        self._price_data = price_data
+        self._neighbors = neighbours
+        self._nbrs = NearestNeighbors(n_neighbors=neighbours, algorithm='ball_tree')
+        self._nbrs.fit(price_data._normalized_intervals);
+
+    def find_nearest(self, datapoint):
+        distances, indices = self._nbrs.kneighbors(datapoint.reshape(1,-1))
+        neighbors_intervals = np.empty([self._neighbors, self._price_data._range])
+        neighbors_future_returns = np.empty([self._neighbors, 4])
+
+        for i in range(self._neighbors):
+            neighbors_intervals[i] = self._price_data._normalized_intervals[indices[0][i]]
+            neighbors_future_returns[i] = self._price_data._future_returns[indices[0][i]]
+
+        return neighbors_intervals, neighbors_future_returns
 
 
 def generate_graph(return_series, stamp):
     fig = plt.plot(return_series, color='k')
     plt.axis('off')
     plt.savefig('graphs/return_chart_' + str(stamp) + '.png')
+    plt.close()
 
-def print_return_matrix(return_matrix):
-    for i in range(return_matrix.shape[0]):
-        generate_graph(return_matrix[i,:], i)
-
-#Returns numpy array instead of this pandas nonsense
-#Most recent date is at bottom of array
-def API_daily_close_time_series(ticker):
-    ts = TimeSeries(key='15BJH4XND0N7L3KL', output_format='pandas')
-    data, meta_data = ts.get_daily(symbol=ticker, outputsize='full')
-    return data['4. close'].values, meta_data
-
-def calculate_30_days_series(daily_returns_matrix):
-    number_intervals = int(daily_returns_matrix.shape[0]/30)
-    series_30_days_trailing = np.empty([number_intervals, 30])
-    #All intervals start at 1
-    series_30_days_trailing[:,0] = 1
-    #Fill the out the 30 day timeseries -->Needs to be trailing
-    interval = 0;
-    day = 0;
-
-    for i in range(number_intervals * 30):
-        if(day == 30):
-            day = 0
-            interval += 1
-        series_30_days_trailing[interval][day] = daily_returns_matrix[i]
-        day += 1
-    return series_30_days_trailing
-
-#Generate the maximum number of sample intervals from a given TimeSeries
-def calculate_intervals(timeseries, interval_length):
-    number_intervals = timeseries.shape[0] - interval_length
-    intervals = np.empty([number_intervals, interval_length])
-
-    interval = 0
-    iteration = 0
-    i = 0;
-
-    while(i < timeseries.shape[0]-1):
-        if(iteration == interval_length):
-            iteration = 0
-            interval += 1
-            i -= interval_length-1
-        intervals[interval][iteration] = timeseries[i]
-        iteration += 1
-        i += 1
-    return intervals
-
-def normalise_time_series(time_series_matrix):
-    number_intervals = time_series_matrix.shape[0]
-    series_length = time_series_matrix.shape[1]
-    return_matrix = np.array(time_series_matrix, copy=True)
-
-    for i in range(number_intervals):
-        base = return_matrix[i][0]
-        for j in range(series_length):
-            return_matrix[i][j] = return_matrix[i][j] / base
-    return return_matrix
-
-def labelled_time_series(return_series):
-    intervals = return_series.shape[0]
-    days = return_series.shape[1]
-    #Need to slice of the last period and add a column for the label
-    labelled_return_series = np.empty([intervals - 1, days + 1])
-    labelled_return_series[:,0:days] = return_series[0:intervals-1,:]
-
-    #Add the subsequents month return as label
-    for i in range(intervals-1):
-        labelled_return_series[i][days] = return_series[i+1][days-1]
-
-    return labelled_return_series
-
-def k_nearest_neighbour_regression(regressor, training_set, labels, neighbors):
-    neigh = KNeighborsRegressor(n_neighbors=neighbors)
-    neigh.fit(training_set, labels)
-    return neigh.predict([regressor])
-
-def test_accuracy(test_set, test_labels, training_set, training_labels, neighbors):
-    same = 0
-    for i in range(test_set.shape[0]):
-        prediction = k_nearest_neighbour_regression(test_set[i,:],training_set, training_labels, neighbors)
-        prediction = prediction*100-100
-        actual = test_labels[i]*100-100
-
-        if(np.sign(prediction) == np.sign(actual)):
-            same +=1
-        print(str(prediction) + ' ' + str(actual))
-    print("Correct sign %: " + str(float(same/len(test_set))))
 
 #Main
-data, meta_data = API_daily_close_time_series('SPX')
-intervals = calculate_intervals(data, 30)
-intervals_normalized = normalise_time_series(intervals)
-intervals_normalized_labelled = labelled_time_series(intervals_normalized)
-# print(intervals_normalized_labelled.shape)
-#
-# print(intervals_normalized_labelled[:,4840:])
-test_values = intervals_normalized_labelled[0:50,]
-training_values = intervals_normalized_labelled[50:4848,]
-#
-test_accuracy(test_values[:,0:30], test_values[:,30], training_values[:,0:30], training_values[:,30], 10)
+prices = PriceData('SPX', 30, 10);
+neighbors = Neighbours(prices, 5)
+results, returns = neighbors.find_nearest(prices._normalized_intervals[100]);
+print(results)
+print(returns)
